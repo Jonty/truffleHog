7ce8cc6a7e8f9fee0633671971758390491152d7
Jonty Wareing <jonty@jonty.co.uk>
2017-07-05 17:20:00 +0100

    Truly awful code to minimise diffs

--- truffleHog/truffleHog.py
+++ truffleHog/truffleHog.py
@@ -3,48 +3,117 @@
 
 import math
 import argparse
-import json
 import re
 from collections import defaultdict
 
 import git
-
 import enchant
 
 # Because the TemporaryDirectory context manager was added in 3.2
 from backports import tempfile
 
+# @@ (source offset, length) (target offset, length) @@ (section header)
+RE_HUNK_HEADER = re.compile(
+    r"^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))?\ @@[ ]?(.*)")
+
+context = 3
+
 def main():
     parser = argparse.ArgumentParser(description='Find secrets hidden in the depths of git.')
-    parser.add_argument('--json', dest="output_json", action="store_true", help="Output in JSON")
     parser.add_argument('git_url', type=str, help='URL for secret searching')
     args = parser.parse_args()
     suspicious_commits = find_strings(args.git_url)
     for commit in suspicious_commits:
-        if args.output_json:
-            print(json.dumps(commit, sort_keys=True, indent=4))
-        else:
-            def cprint(string, col):
-                print(col + string + colour.ENDC)
+        def cprint(string, col):
+            print(col + string + colour.ENDC)
 
-            cprint('commit %s' % commit['commit_sha'], colour.YELLOW)
-            print('Author: %s' % commit['author'])
-            print('Date:   %s' % commit['date'].strftime('%Y-%m-%d %H:%M:%S') + ' (%s)' % (commit['author_tz_offset']/60/60))
-            print
-            print('    %s' % commit['short_message'])
-            print
-            
-            for blob in commit['suspicious_blobs']:
-                cprint('index %s' % blob['hexes'], colour.YELLOW)
-                cprint('--- a/%s' % blob['path'], colour.BOLD)
-                cprint('+++ b/%s' % blob['path'], colour.BOLD)
-                highlighted_diff = blob['diff']
+        cprint('commit %s' % commit['commit_sha'], colour.YELLOW)
+        print('Author: %s' % commit['author'])
+        print('Date:   %s' % commit['date'].strftime('%Y-%m-%d %H:%M:%S') + ' (%s)' % (commit['author_tz_offset']/60/60))
+        print
+        print('    %s' % commit['short_message'])
+        print
+        
+        for blob in commit['suspicious_blobs']:
+            #cprint('index FOO..FOO BAR', colour.BOLD)
+            cprint('diff --git a/%s b/%s' % (blob['path'], blob['path']), colour.BOLD)
+            cprint('--- a/%s' % blob['path'], colour.BOLD)
+            cprint('+++ b/%s' % blob['path'], colour.BOLD)
+
+            hunk = None
+            line_no = None
+
+            def print_hunk(hunk):
+                if hunk is not None and hunk['highlight_lines']:
+                    lines_to_output = set()
+                    for offset in hunk['highlight_lines']:
+                        start = offset-context
+                        end = offset+context+1
+                        if start < 0:
+                            start = 0
+                        if end > len(hunk['lines']):
+                            end = len(hunk['lines'])
+                        lines_to_output.update(range(start, end))
+
+                    line_list = sorted(list(lines_to_output))
+
+                    index = None
+                    lines_removed = lines_added = lines_context = 0
+                    for offset in line_list:
+                        if hunk['lines'][offset].startswith('-'):
+                            lines_removed += 1
+                        elif hunk['lines'][offset].startswith('+'):
+                            lines_added += 1
+                        else:
+                            lines_context += 1
+
+                    for offset in line_list:
+                        if offset-1 != index:
+                            if index:
+                                print # Space between hunks
+                            index = offset
+
+                            start = hunk['source'] + offset - context
+                            if start < 0:
+                                start = 0
+
+                            cprint("@@ -%d,%d %d,%d @@" % (
+                                start,
+                                lines_context + lines_removed,
+                                start,
+                                lines_context + (lines_added - lines_removed),
+                            ), colour.CYAN)
+                        else:
+                            index += 1
+
+                        if hunk['lines'][offset].strip():
+                            print(hunk['lines'][offset])
+                    print
+
+            for line in blob['diff'].splitlines():
+                header_info = RE_HUNK_HEADER.match(line)
+                if header_info:
+                    print_hunk(hunk)
+
+                    line_no = 0
+                    source, s_len, target, t_len, _ = header_info.groups()
+                    hunk = {
+                        'source': int(source),
+                        'target': int(target),
+                        'lines': [],
+                        'highlight_lines': set(),
+                    }
+                    continue
+
                 for string in blob['suspicious_strings']:
-                    highlighted_diff = highlighted_diff.replace(string, colour.BOLD + colour.RED + string + colour.ENDC)
-                
-                first_line = blob['diff'].splitlines()[0]
-                highlighted_diff = highlighted_diff.replace(first_line, colour.CYAN + first_line + colour.ENDC)
-                print(highlighted_diff)
+                    if string in line:
+                        line = line.replace(string, colour.BOLD + colour.RED + string + colour.ENDC)
+                        hunk['highlight_lines'].add(line_no)
+
+                hunk['lines'].append(line)
+                line_no += 1
+
+            print_hunk(hunk)
 
 
 BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
@@ -87,7 +156,6 @@ def find_suspicious_strings(line):
 
     base64_strings = strings_matching(line, BASE64_CHARS)
     for string in base64_strings:
-        #print(string, shannon_entropy(string))
         if shannon_entropy(string) > 4.0 and not is_probably_language(string):
             found.append(string)
 
